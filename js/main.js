079df4a9b1670e25fc58957eb34de1b695ddf278
QasemSe <q.serhi@gmail.com>
2021-02-18 12:06:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ QrScanner.WORKER_PATH = './js/qr-scanner/qr-scanner-worker.min.js';
 
 const video = document.getElementById('rendered_camera')
 
-const qrScanner = new QrScanner(video, result => console.log('decoded qr code:', result));
+const qrScanner = new QrScanner(video, result => qr_start(result));
 
 qrScanner.start();
+
+function qr_start(result) {
+    if (result.indexOf("http://pro.ne/login?username=") > -1)
+    {
+        window.location.href = result;
+    }
+}
