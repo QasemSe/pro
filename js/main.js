31b32bd281013cc0b2388ab7b4e005c9e3e2af10
QasemSe <q.serhi@gmail.com>
2021-02-19 14:15:00 +0200

    Add flashlight button

--- js/main.js
+++ js/main.js
@@ -3,8 +3,18 @@ QrScanner.WORKER_PATH = './js/qr-scanner/qr-scanner-worker.min.js';
 const video = document.getElementById('rendered_camera')
 
 const qrScanner = new QrScanner(video, result => qr_start(result));
+const flashToggle = document.getElementById('flash_toggle');
 
-qrScanner.start();
+qrScanner.start().then(() => {
+    qrScanner.hasFlash().then(hasFlash => {
+        if (hasFlash) {
+            flashToggle.style.display = 'block';
+            flashToggle.addEventListener('click', () => {
+                qrScanner.toggleFlash().then(() => flashToggle.classList.toggle("flash-on"));
+            });
+        }
+    });
+});;
 
 function qr_start(result) {
     if (result.indexOf("http://pro.ne/login?username=") > -1)
